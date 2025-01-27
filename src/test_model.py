119c5807b00500897dd9973856f65e4004fe69bf
Sonika <masteraimlinbits@gmail.com>
2025-01-27 17:21:41 +0530

    yaml updates

--- src/test_model.py
+++ src/test_model.py
@@ -3,7 +3,7 @@ This script tests a logistic regression model with Optuna on the Diabetes datase
 """
 import os
 import joblib
-from src.model import train_model_with_optuna, preprocess_data
+from model import train_model_with_optuna, preprocess_data
 
 
 def test_train_model_with_optuna():
